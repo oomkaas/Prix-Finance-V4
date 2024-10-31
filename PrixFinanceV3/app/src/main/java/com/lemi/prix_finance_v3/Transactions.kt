a20bf04cabfdaf78096e0928a9bc92b975df2737
lemi-manqoyi <liyemamanqoyi23+GITHUB@gmail.com>
2024-10-31 12:10:20 +0200

    login pass hide. ui fixes

---
+++ PrixFinanceV3/app/src/main/java/com/lemi/prix_finance_v3/Transactions.kt
@@ -0,0 +1,20 @@
+package com.lemi.prix_finance_v3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Transactions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_transactions)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
